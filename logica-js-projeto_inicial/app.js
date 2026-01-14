8689e46495b3c300969cc77cfbc8c48b1a53e1b9
murilo.sousa <murilo.rodis545@gmail.com>
2026-01-14 09:38:43 -0300

    Estudando, essa é a primeira e segunda aula

---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,71 @@
+// AULA 1 //
+
+alert('Boas vindas ao jogo do Número Secreto!');
+
+let numeroSecreto = 29;
+console.log(numeroSecreto)
+let chute = prompt('Escolha um número entre 1 e 30');
+
+if (numeroSecreto == chute) {
+    console.log(`Parabéns, você descobriu o número secreto! (29)`)
+}
+
+// atividade // são exercícios separados!
+
+alert('Boas vindas ao nosso site!');
+
+let nome = "Lua";
+
+let idade = 25;
+
+let numeroDeVendas = 50;
+
+let saldoDisponivel = 1000;
+
+alert("Erro! Preencha todos os campos");
+
+let mensagemDeErro = ("Erro! Preencha todos os campos");
+alert(mensagemDeErro);
+
+let nomeDoUsuario = prompt("Digite o seu nome")/
+
+let idade = prompt("Digite a sua idade");
+
+let idade = prompt("Digite a sua idade");
+if (idade >= 18) {
+    alert("Pode tirar habilitação!");
+}
+
+// AULA 2 // CONTINUAÇÃO DO CÓDIGO DA PRIMEIRA AULA
+
+alert('Boas vindas ao jogo do Número Secreto!');
+
+let numeroSecreto = 29;
+let chute = prompt('Escolha um número entre 1 e 30');
+
+if (chute == numeroSecreto) {
+    console.log(`Parabéns, você descobriu o número secreto! ${numeroSecreto}
+        `);
+} else{
+    alert("Você errou: ")
+} 
+
+// atividades aula 2 
+
+let fimdeSemana = sábado , domingo // errado
+
+let resposta = prompt("Qual é o dia semana?");
+if (resposta == fimdeSemana)
+    alert("Bom fim de semana!");
+else{
+    alert("Boa semana!");
+}
+
+diaDaSemana = prompt('Qual é o dia da semana?'); // certo
+if (diaDaSemana == 'Sábado') {
+    alert('Bom fim de semana!');
+} else if (diaDaSemana == 'Domingo') {
+    alert('Bom fim de semana!');
+} else {
+    alert('Boa semana!');
+}
