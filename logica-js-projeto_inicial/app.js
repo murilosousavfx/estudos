9ba92ec0c6fa5577a5fc54a73ab312afae04fa16
murilo.sousa <murilo.rodis545@gmail.com>
2026-01-16 09:15:56 -0300

    adicionando novas condições

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -43,11 +43,15 @@ alert('Boas vindas ao jogo do Número Secreto!');
 let numeroSecreto = 29;
 let chute = prompt('Escolha um número entre 1 e 30');
 
+
 if (chute == numeroSecreto) {
-    console.log(`Parabéns, você descobriu o número secreto! ${numeroSecreto}
-        `);
-} else{
-    alert("Você errou: ")
+    alert(`Parabéns, você descobriu o número secreto! ${numeroSecreto}`);
+} else {
+    if (chute > numeroSecreto) {
+        alert(`O número secreto é menor que ${chute}`);
+    } else {
+        alert(`O número secreto é maior que ${chute}`)´;
+    }
 } 
 
 // atividades aula 2 
@@ -90,3 +94,5 @@ alert(`Seu saldo é de R$${saldoConta}.`);
 
 let nome = prompt("Digite o seu nome: ");
 alert(´Bem vindo ao jogo ${nome}´);
+
+// AULA 3
